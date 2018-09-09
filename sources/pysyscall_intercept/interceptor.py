35d3e717dca05f0aadf1d5b56c5813f0394e989e
Lukasz Laszko <lukaszlaszko@gmail.com>
2018-09-09 13:25:26 +0800

    interceptor enabled for pdb

--- sources/pysyscall_intercept/interceptor.py
+++ sources/pysyscall_intercept/interceptor.py
@@ -1,1 +1,19 @@
-from _interceptor import *
+from contextlib import contextmanager
+from signal import getsignal, signal, SIGTRAP
+
+
+@contextmanager
+def block_signals(signals):
+    def empty_handler(signo, frame):
+        pass
+
+    handler = getsignal(SIGTRAP)
+    signal(SIGTRAP, empty_handler)
+    try:
+        yield
+    finally:
+        signal(SIGTRAP, handler)
+
+
+with block_signals({SIGTRAP}):
+    from _interceptor import *
